98a29d2e689d9e42a49d07658f8fd68b83c2fc48
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-24 17:43:30 +0500

    add

--- db_managment/articles/models.py
+++ db_managment/articles/models.py
@@ -1,45 +1,49 @@
-from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+from django.db import models
 
+# Create your models here.
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
-
-    def __str__(self):
+    
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        
+    def str(self):
         return str(self.name)
-
-
+    
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
-
-    def __str__(self):
+    
+    def str(self):
         return str(self.name)
+    
 
-
-class Aricles(models.Model):
+class Articles(models.Model):
     title = models.CharField(max_length=300)
-    slug = models.SlugField(max_length=100, unique=True)
-    tag = models.ManyToManyField(Tag, related_name="tags")
-    category = models.ForeignKey(category, on_delete=models.CASCADE, related_name='categories')
-    author = models.ForeignKey(User, on_delete=models.SET_DEFAULT)
+    slug = models.SlugField(max_length=300, unique=True)
+    tag = models.ManyToManyField(Tag, related_name='tags')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='categories')
+    author = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     body = models.TextField()
-    rating = models.models.PositiveIntegerField(default=0)
+    rating = models.PositiveIntegerField(default=0)
     published = models.BooleanField(default=False)
+    published_date = models.DateTimeField(auto_now_add=True)
     on_top = models.BooleanField(default=False)
-    comments = models.PositiveIntegerField(default=0)
-
-    def __str__(self):
+    comments = models.PositiveBigIntegerField(default=0)
+    views = models.PositiveBigIntegerField(default=0)
+    
+    
+    def str(self):
         return str(self.title)
-
-
+    
 class Comment(models.Model):
-    article = models.ForeignKey(
-        Article, on_delete=models.CASCADE, related_name='comments')
-    user = models.ForeignKey(User, on_delete=models.SET_DEFAULT)
+    article = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name='post_comments')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     comment = models.TextField()
-
-    def __str__(self):
+    
+    def str(self):
         return str(self.user.username)
