e03877bd3f25db4687ec9b86b340a20ae326c472
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-27 16:16:15 +0500

    add

--- db_managment/articles/models.py
+++ db_managment/articles/models.py
@@ -1,8 +1,10 @@
-from django.contrib.auth.models import User
 from django.db import models
+from django.contrib.auth.models import User
+
+# from django.template.defaultfilters import slugify
+
 
 # Create your models here.
-
 class Category(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
@@ -10,40 +12,39 @@ class Category(models.Model):
     class Meta:
         verbose_name = "Category"
         verbose_name_plural = "Categories"
-        
-    def str(self):
+    
+    def __str__(self):
         return str(self.name)
     
 class Tag(models.Model):
     name = models.CharField(max_length=100)
     slug = models.SlugField(max_length=100, unique=True)
     
-    def str(self):
+    def __str__(self):
         return str(self.name)
     
-
-class Articles(models.Model):
+class Article(models.Model):
     title = models.CharField(max_length=300)
     slug = models.SlugField(max_length=300, unique=True)
-    tag = models.ManyToManyField(Tag, related_name='tags')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE,related_name='categories')
+    tag = models.ManyToManyField(Tag, related_name="tags")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='categories')
     author = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     body = models.TextField()
     rating = models.PositiveIntegerField(default=0)
     published = models.BooleanField(default=False)
     published_date = models.DateTimeField(auto_now_add=True)
     on_top = models.BooleanField(default=False)
-    comments = models.PositiveBigIntegerField(default=0)
-    views = models.PositiveBigIntegerField(default=0)
+    comments = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
     
-    
-    def str(self):
+    def __str__(self):
         return str(self.title)
-    
+
 class Comment(models.Model):
-    article = models.ForeignKey(Articles, on_delete=models.CASCADE, related_name='post_comments')
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, related_name='post_comments')
     user = models.ForeignKey(User, on_delete=models.PROTECT, null=True)
     comment = models.TextField()
     
-    def str(self):
+    def __str__(self):
         return str(self.user.username)
+    
