0916e63a7172fc373b54ed695bd3a40980e32165
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-29 16:04:49 +0500

    add

--- db_managment/articles/views.py
+++ db_managment/articles/views.py
@@ -1,29 +1,26 @@
 from django.shortcuts import render
 
 from .models import *
+
 # Create your views here.
-# def all_articles_view(request):
-#     return render(request, "articles/posts.html")
-
-
 def all_articles_view(request):
+    all_categories = Category.objects.all()
     all_posts = Article.objects.all()
-    category = Category.objects.all()
+    
     data = {
-        'posts': all_posts,
-        'category': all_categories
+        'posts':all_posts,
+        'categories':all_categories
     }
-
+    
     return render(request, "articles/posts.html", context=data)
 
-
-def post_detail(request, pk):
-    article = Article.objects.get(id=pk)
-
-    return render(request, 'articles/detail.html', context={"object": article})
-
+def post_detail(request,pk):
+    article = Article.objects.get(id=pk)    
+    return render(request, 'articles/detail.html', context={"object":article})
 
 def category_list(request, category_slug):
-    category = Category.objects.get(name=category_slug)
-    articles = Articles.objects.filter(category=category)
+    category = Category.objects.get(slug=category_slug)
+    articles = Article.objects.filter(category=category)
     return render(request, 'articles/category_posts.html', context={"posts":articles})
+    # comm
+    
