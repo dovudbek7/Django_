e3b0b42aac683c0f317680c2e907d46db35874c6
dovudbek7 <111281179+dovudbek7@users.noreply.github.com>
2023-03-31 16:00:28 +0500

    add

--- db_managment/articles/utils.py
+++ db_managment/articles/utils.py
@@ -1,21 +1,20 @@
-def check_article_view():
+def check_read_articles(request):
     try:
-        return request.session["read_articles"]
+        read_articles = request.session["read_articles"]
     except:
         request.session["read_articles"] = []
-        return request.session["read_articles"] 
-    else:
-        return request.sesssion["read_articles"]
+        read_articles = request.session.get("read_articles")
+    return read_articles
         
-def chek_article_view(request, article_id):
+
+
+def check_article_view(request, article_id):
     request.session.modified = True
     a_list = check_read_articles(request)
-    if aricle_id in a_list:
-        print("article read...")
+    if article_id in a_list:
+        print("article read..")
         return False
     else:
         a_list.append(article_id)
-        print("article views updtaed")
+        print("article views updated")
         return True
-    
-    
--- db_managment/articles/views.py
+++ db_managment/articles/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
 from .models import *
-from . import utils
+from .utils import check_article_view
+
+# Create your views here.
 def all_articles_view(request):
+    # request.session.clear()
+    # print(request.session.get("read_articles"))
+    
+    
+    
     all_categories = Category.objects.all()
     all_posts = Article.objects.all()
     
@@ -14,19 +21,23 @@ def all_articles_view(request):
     return render(request, "articles/posts.html", context=data)
 
 
-        
-def post_detail(request,pk):
-    article = Article.objects.get(slug=article_slug)
+
+
+
+def post_detail(request,article_slug):
+    article = Article.objects.get(slug=article_slug)  
     
     if check_article_view(request,article.id):
         article.views += 1
         article.save()
     else:
-        pass
+        pass  
+
     return render(request, 'articles/detail.html', context={"object":article})
 
 def category_list(request, category_slug):
     category = Category.objects.get(slug=category_slug)
     articles = Article.objects.filter(category=category)
     return render(request, 'articles/category_posts.html', context={"posts":articles})
+    # comm
     
